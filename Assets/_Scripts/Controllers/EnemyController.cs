a3c0e0b61186ded136b5001f7c3d131b5b5e3847
enes-ozdemir <enesozdemir6167@hotmail.com>
2024-02-29 00:51:46 +0300

    Enemy controller changes

--- Assets/_Scripts/Controllers/EnemyController.cs
+++ Assets/_Scripts/Controllers/EnemyController.cs
@@ -1,17 +1,35 @@
-﻿using MalbersAnimations;
+﻿using EmeraldAI;
+using MalbersAnimations;
+using UnityEngine;
 
 namespace _Scripts.Controllers
 {
     public class EnemyController : CharController
     {
         MDamageable mDamageable;
-        
+        Stats stats;
+        EmeraldAISystem mEmeraldAISystem;
         
         private void Start()
         {
             mDamageable = GetComponent<MDamageable>();
+            mEmeraldAISystem = GetComponent<EmeraldAISystem>();
+            mDamageable.events.OnReceivingDamage.AddListener(OnReceiveDamage);
+            mEmeraldAISystem.DeathEvent.AddListener(OnDeath);
+            
             // mDamageable.OnReceiveDamage += OnReceiveDamage;
             // mDamageable.stats.PinnedStat.
+        }
+
+        private void OnDeath()
+        {
+            Debug.Log("OnDeath");
+        }
+
+        private void OnReceiveDamage(float arg0)
+        {
+            Debug.Log($"{mEmeraldAISystem.CurrentHealth} OnReceiveDamage: {arg0}");
+            mEmeraldAISystem.Damage(10);
         }
     }
 }
